8421434d8d13ba671ef44f8f385187806aea82c5
AbbasIman <85749516+Cabbasullah@users.noreply.github.com>
2022-12-18 00:12:25 +0700

    Add files via upload

---
+++ validation/main.py
@@ -0,0 +1,46 @@
+from subprocess import *
+import numpy as np
+import random
+import subprocess
+import numpy
+
+M = np.random.randint(13, size=(2, 2))
+v = np.random.randint(13,size=(2,1))
+
+MV=numpy.append(M,v)
+mvc=MV.flatten()
+
+print("from python:")
+print(f"v =\n {v}\n M= \n{M}\n")
+print(f"MV={MV}\n")
+
+def correct_function(M,v):
+    return np.dot(M,v)
+
+list_input_to_c = ["./main.out"]
+
+for matrix_vector_value in mvc:
+    list_input_to_c.append(str(matrix_vector_value))
+
+#subprocess.call(list_input_to_c)
+process = subprocess.run(list_input_to_c,universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#print(process.stdout.split(","))
+mvc=[]
+mv=process.stdout.split(",")
+for i in mv:
+    mvc.append(int(i))
+
+
+
+
+mvp = correct_function(M,v)
+#print(f"\nFROM PYTHON: \n{mvp}")
+
+
+if np.all(mvp)==np.all(mvc):
+   print("Output Validation Succeeded")
+else:
+   print("validation failed")
+
+
+
